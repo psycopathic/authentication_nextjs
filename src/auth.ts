8dae306e267e86e7766d2033a6ac301a54ece88c
psycopathic <harshsrivastava2502@gmail.com>
2025-09-02 01:10:05 +0530

    pages being made

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,11 @@ import NextAuth, { CredentialsSignin } from "next-auth";
 import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 import User from "./models/userModel";
+import bcrypt from "bcryptjs";
+
+//connection to db 
+// making of custome login and signup pages
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -20,21 +25,31 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         },
         password: { label: "Password", type: "password" },
       },
-      authorize: async (credentials)=> {
+      authorize: async (credentials) => {
         if (!credentials) return null;
 
         const email = credentials.email as string | undefined;
         const password = credentials.password as string | undefined;
 
-        if (!email || !password) throw new CredentialsSignin("please provide both email and password");
+        if (!email || !password)
+          throw new CredentialsSignin("please provide both email and password");
 
         console.log("Email:", email);
         console.log("Password:", password);
 
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }).select("+password");
+        if (!user)
+          throw new CredentialsSignin("No user found with the given email");
+
+        if (!user.password)
+          throw new CredentialsSignin("No password found for the user");
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) throw new CredentialsSignin("Password does not match");
 
         if (password !== "passcode") {
-          throw new Error("Password does not match");
+          throw new CredentialsSignin("Password does not match");
         }
 
         return user;
@@ -42,5 +57,3 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
 });
-
-
